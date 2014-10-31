6a591df5f553e3f8b838c79b28618e087abf0b8e
Rock(Fei Liu) <rock.lau889@gmail.com>
2014-10-31 18:03:27 +0800

    require added

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by rock on 31/10/14.
+ */
+require.config({
+    paths: {
+        jquery: 'jquery-1.8.2.min',
+        less: 'less-1.7.5.min'
+    }
+});
+
+require(['jquery','less'], function($) {
+    $(function(){
+
+    });
+});
